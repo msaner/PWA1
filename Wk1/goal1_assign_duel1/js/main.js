a154463e3ca5a8e3743115da797d96189f666a46
msaner <msaner@fullsail.edu>
2013-10-30 20:49:53 -0700

    added variables and functions to set up the program

--- Wk1/goal1_assign_duel1/js/main.js
+++ Wk1/goal1_assign_duel1/js/main.js
@@ -4,8 +4,39 @@ DATE: 10/30/2013
 ASSIGNMENT: Goal 1 Duel 1
 */
 
+
 //this is a self executing function
 (function(){
 	console.log("FIGHT!");
+	// player names
+	var playerOneName = "Spiderman";
+	var playerTwoName = "Batman";
 	
+	// the players starting health level
+	var playerOneHealth = 100;
+	var playerTwoHealth = 100;
+	
+	// the max amount of damage per round the player can cause
+	var playerOneDamage = 23;
+	var playerTwoDamage = 23;
+	
+	// round number of the game
+	var round = 1;
+	
+	// fight function
+	function fight(){
+		
+		// calls the winnerCheck function
+		winnerCheck()
+	};
+	
+	// check for a winner
+	function winnerCheck(){
+		
+	};
+	
+	// call (aka run) function check. This is where the program starts
+	fight();
+
 })();
+
