0ef44272fca634ceca38594ce1309e2caafa9834
msaner <msaner@fullsail.edu>
2013-11-06 21:31:17 -0800

    added objects
    created two objects for player1 and 2.  updated console.log and
    argument statements in fight function

---
+++ Wk2/goal4_assign_duel3/js/main.js
@@ -0,0 +1,87 @@
+/*
+NAME: Michael Saner
+DATE: 11/6/2013
+ASSIGNMENT: Goal 4 Duel 3
+*/
+
+
+//this is a self executing function
+(function(){
+	console.log("FIGHT!");
+	// player names
+/*var playerOneName = "Spiderman";
+	var playerTwoName = "Batman";
+	
+	// the players starting health level
+	var playerOneHealth = 100;
+	var playerTwoHealth = 100;
+	
+	// the max amount of damage per round the player can cause
+	var playerOneDamage = 23;
+	var playerTwoDamage = 23;
+
+	var playerOne = ["Spiderman", 100, 23]; // 0=name, 1=health, 2=damage
+	var playerTwo = ["Batman", 100, 23]; // 0=name, 1=health, 2=damage
+*/
+	var playerOne = {name:'Spiderman', health:100, damage:23};
+	var playerTwo = {name:'Batman', health:100, damage:23};
+	console.log(playerOne.name + ", player2= " + playerTwo.name);
+	
+	// round number of the game
+	var round = 0;
+	
+	// fight function
+	function fight(playerOne, playerTwo){
+		console.log('pass fight function arguments: ' + playerOne.name, playerTwo.name);
+		alert(playerOne[0] + ":" + playerOne[1] + " *START* " + playerTwo[0] + ":" + playerTwo[1]);
+		
+		
+		// basic "for loop"
+		for(var i = 0; i < 10; i++){
+			// generate a random number
+			
+			var minDamage1 = playerOne[2] * .5;
+			var minDamage2 = playerTwo[2] * .5;
+			var f1 = Math.floor(Math.random() * (playerOne[2] - minDamage1) + minDamage1);
+			var f2 = Math.floor(Math.random() * (playerTwo[2] - minDamage2) + minDamage2);
+			
+			// inflicted damage formula
+			playerOne[1] -= f1;
+			playerTwo[1] -= f2;
+			
+			console.log(playerOne[0] + ":" + playerOne[1] + "  " + playerTwo[0] + ":" + playerTwo[1]);	
+			
+			// calls the winnerCheck function
+			var result = winnerCheck()
+			console.log(result);	
+			
+			if (result === "no winner"){
+				round++;
+				alert(playerOne[0] + ":" + playerOne[1] + " *ROUND " + round + " OVER*"  + playerTwo[0] + ":" + playerTwo[1]);
+			}else{
+				alert(result);
+				break; // exit out of the for loop
+			};
+		};		
+	};
+	
+	// check for a winner
+	function winnerCheck(){
+		var result = "no winner";
+		
+		if(playerOne[1] < 1 && playerTwo[1] < 1){
+			result = "You Both Die";			
+		}else if(playerOne[1] < 1){
+			result = playerTwo[0] + " WINS!!!"
+		}else if(playerTwo[1] < 1){
+			result = playerOne[0] + " WINS!!!"
+		}; //end of if, else if, else if
+		return result;
+		
+	};  // end winnerCheck function
+	
+	// call (aka run) function check. This is where the program starts
+	fight(playerOne, playerTwo);
+
+})();
+
