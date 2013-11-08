11d0786e39aaa01c930e4546eeda0e54aec19694
msaner <msaner@fullsail.edu>
2013-11-07 23:04:36 -0800

    everything but the round counter
    added all code to display the player names and health.  deleting a div
    element when a winner is decided so that result of game shows up
    sentered in the white bar

--- Wk2/goal4_assign_duel3/js/main.js
+++ Wk2/goal4_assign_duel3/js/main.js
@@ -7,12 +7,6 @@ ASSIGNMENT: Goal 4 Duel 3
 
 //this is a self executing function
 (function(){
-	console.log("FIGHT!");
-	// player names
-/*
-	var fighters.players[0] = ["Spiderman", 100, 23]; // 0=name, 1=health, 2=damage
-	var fighters.players[1] = ["Batman", 100, 23]; // 0=name, 1=health, 2=damage
-*/
 
 	var fighters = { 
 		players:[
@@ -20,59 +14,71 @@ ASSIGNMENT: Goal 4 Duel 3
 			{name:'Batman', health:100, damage:23}
 		]
 	};
-	console.log("should be spiderman " + fighters.players[0].name + ", player2= " + fighters.players[1].name);
 	
 	// round number of the game
 	var round = 0;
 	
 	// fight function
 	function fight(fighters){
-		console.log('pass fight function arguments: ' + fighters.players[0].name, fighters.players[1].name);
-		//alert(fighters.players[0].name + ":" + fighters.players[0].health + " *START* " + fighters.players[1].name + ":" + fighters.players[1].health);
+		document.querySelector('#fight_btn').onclick = function(e){
 	
-	// get spidermans name and health in the top left
-var kabal = document.querySelector('#kabal p'); // makes var = p tag inside of element with id of kabal
-console.log(kabal);
-kabal.innerHTML = fighters.players[0].name + ":" + fighters.players[0].health;	 //puts the code inside of the p tag 
-
-// Get Batmans name and health in the top right 
-var kratos = document.querySelector('#kratos p');
-console.log(kratos);
-kratos.innerHTML = fighters.players[1].name + ":" + fighters.players[1].health;	
-	
-		// basic "for loop"
-		for(var i = 0; i < 10; i++){
-			// generate a random number
-			
-			var minDamage1 = fighters.players[0].damage * .5;
-			var minDamage2 = fighters.players[1].damage * .5;
-			var f1 = Math.floor(Math.random() * (fighters.players[0].damage - minDamage1) + minDamage1);
-			var f2 = Math.floor(Math.random() * (fighters.players[1].damage - minDamage2) + minDamage2);
-			
-			// inflicted damage formula
-			fighters.players[0].health-= f1;
-			fighters.players[1].health -= f2;
-			
-			
-			console.log(fighters.players[0].name + ":" + fighters.players[0].health + "  " + fighters.players[1].name + ":" + fighters.players[1].health);	
-			
-			
+			// get spidermans name and health in the top left
+			var kabal = document.querySelector('#kabal p'); // makes var = p tag inside of element with id of kabal
+			kabal.innerHTML = fighters.players[0].name + ":" + fighters.players[0].health;	 //puts the code inside of the p tag 
 		
-			// calls the winnerCheck function
-			var result = winnerCheck()
-			console.log(result);	
+			// Get Batmans name and health in the top right 
+			var kratos = document.querySelector('#kratos p');
+			kratos.innerHTML = fighters.players[1].name + ":" + fighters.players[1].health;	
+	
+			// basic "for loop"
+			for(var i = 0; i < 10; i++){
+				// generate a random number
+				var minDamage1 = fighters.players[0].damage * .5;
+				var minDamage2 = fighters.players[1].damage * .5;
+				var f1 = Math.floor(Math.random() * (fighters.players[0].damage - minDamage1) + minDamage1);
+				var f2 = Math.floor(Math.random() * (fighters.players[1].damage - minDamage2) + minDamage2);
+				
+				// inflicted damage formula
+				fighters.players[0].health-= f1;
+				fighters.players[1].health -= f2;
+				
+				console.log(fighters.players[0].name + ":" + fighters.players[0].health + "  " + fighters.players[1].name + ":" + fighters.players[1].health);	
+				
+				// get spidermans name and health in the top left
+				var kabal = document.querySelector('#kabal p'); // makes var = p tag inside of element with id of kabal
+				kabal.innerHTML = fighters.players[0].name + ":" + fighters.players[0].health;	 //puts the code inside of the p tag 
 			
-			if (result === "no winner"){
-				round++;
-				var counter = document.getElementById('round');
-				counter.innerHTML = round;
-			//alert(fighters.players[0].name + ":" + fighters.players[0].health + " *ROUND " + round + " OVER*"  + fighters.players[1].name + ":" + fighters.players[1].health);
-			}else{
-				//alert(result);
-				var counter = document.getElementById('round');
-				counter.innerHTML = result;
-				break; // exit out of the for loop
+				// Get Batmans name and health in the top right 
+				var kratos = document.querySelector('#kratos p');
+				kratos.innerHTML = fighters.players[1].name + ":" + fighters.players[1].health;	
+			
+				// calls the winnerCheck function
+				var result = winnerCheck()
+				console.log(result);	
+				
+				if (result === "no winner"){
+					round++;
+					
+					var kabal = document.querySelector('#kabal p'); // makes var = p tag inside of element with id of kabal
+					kabal.innerHTML = fighters.players[0].name + ":" + fighters.players[0].health;	 //puts the code inside of the p tag 
+			
+					// Get Batmans name and health in the top right 
+					var kratos = document.querySelector('#kratos p');
+					kratos.innerHTML = fighters.players[1].name + ":" + fighters.players[1].health;	
+	
+				//alert(fighters.players[0].name + ":" + fighters.players[0].health + " *ROUND " + round + " OVER*"  + fighters.players[1].name + ":" + fighters.players[1].health);
+				}else{
+					//alert(result);
+					var win = document.querySelector('#kratos p');
+					win.innerHTML = result;
+					var hide = document.querySelector('#kabal');
+					hide.remove(hide.p);
+					break; // exit out of the for loop
+				};
+			e.preventDefault();
+			return false;
 			};
+			
 		};		
 	};
 	
