6130dc55dfcfbb90737ac1dcf39575aee477f6b0
msaner <msaner@fullsail.edu>
2013-11-08 00:45:30 -0800

    set up object and vars
    added variables to hold inputs, generating random number, passing
    things into a funciton

--- Wk2/goal4_assign_guess/js/main.js
+++ Wk2/goal4_assign_guess/js/main.js
@@ -9,6 +9,34 @@
 
 //Game variables
 (function (){
-
+	
+	// generate random number from 1-10
+	var ranNum = Math.floor(1 + Math.random()*10);
+	console.log(ranNum);
+	
+	
+	var values = {
+		input: document.querySelector('#input'),
+		output: document.querySelector('#output'),
+		button: document.querySelector('button')
+	};
+	console.log(values[0], values[1], values[2]);
+	
+	var listener = document.querySelector('button');
+	listener.addEventListener('onclick', validateInput, false);
+	
+	 function validateInput(ranNum, values){
+		console.log('function validateInput is running');
+		console.log("random number " + ranNum + " made it inside the function");
+				
+		playerGuess = parseInt(values.input.value);
+		
+		if (isNaN(playerGuess)){
+			var NotValidInput = document.getElementById(output);
+			NotValidInput.innerHTML('That is not a valid guess. Enter a number 1-10');
+		};
+	};
+	
+	var validateInput = validateInput(ranNum, values);
 
 })();
