313c53e86bd486b6edb23cc754146bdb7c4e9d52
msaner <msaner@fullsail.edu>
2013-11-15 19:57:45 -0800

    fixed person object
    added random number to choose action and job fixed the person object

--- Wk3/goal7_assign_objects/js/person.js
+++ Wk3/goal7_assign_objects/js/person.js
@@ -9,11 +9,12 @@
  */
 
 
-	var Person = function(name, action, job, row){
+	var Person = function(name, row){
 			this.name = name;
-			this.action = action;
-			this.job = job;
+			this.action = this.actions[Math.floor(Math.random()*(this.actions.length - 0) + 0)];
+			this.job = this.jobs[Math.floor(Math.random()*(this.jobs.length - 0) + 0)];
 			this.row = row;
+			
 		};
 	
 		Person.prototype.jobs = ["teacher","farmer","student","pilot"];
