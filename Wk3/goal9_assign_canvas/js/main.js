2a218f3f90f05072c59f81eca07c063135b9034e
msaner <msaner@fullsail.edu>
2013-11-16 21:14:35 -0800

    setting up canvas and function to create img
    added numbers to chartData array, function to create an image begin
    setting up a function to create the bars for the graph

--- Wk3/goal9_assign_canvas/js/main.js
+++ Wk3/goal9_assign_canvas/js/main.js
@@ -22,4 +22,36 @@ console.log("start canvas");
 	//add the canvas to the HTML document
 	document.body.appendChild(canvas);
 
+	// set the method to allow drwaing objects on the canvas
+	var ctx = canvas.getContext('2d');
+	
+	// variable to store an array of 4 numbers
+	var chartData = [
+		23,
+		56,
+		98,
+		47
+	];
+	var maxBarHeight = 200;
+	
+	// create an image
+	var img = new Image();
+	
+	// define location of image file
+	img.src = "images/bg.jpg";
+	
+	// create the image and wait to load before instantiating it
+	img.addEventListener('load', function(){
+		ctx.drawImage(img, 0, 0, 400, 300)
+		
+		drawBars(chartData);
+			
+	}, false);
+	
+	var drawBars = function(){
+		// set the font used for the numbers of each bar on the graph
+			ctx.font = '14px Georgia'
+		
+	};
+	
 })();
