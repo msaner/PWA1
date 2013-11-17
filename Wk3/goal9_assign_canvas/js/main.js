bb12f1a1fa469ad105cbba6df728105dd2266220
msaner <msaner@fullsail.edu>
2013-11-16 20:30:34 -0800

    added canvas to DOM
    created a canvas dynamically set it's dimensions and added it to the DOM

--- Wk3/goal9_assign_canvas/js/main.js
+++ Wk3/goal9_assign_canvas/js/main.js
@@ -2,11 +2,24 @@
  * Created by:
  * Class: PWA1
  * Goal: Goal9
+ 
+ 	Michael Saner
+	11-16-2013
+	Canvas
  */
 
 console.log("start canvas");
 
 (function(){
     
+	// dynamically create a canvas
+	var canvas = document.createElement("canvas");
+	
+	// set the size of the canvas
+	canvas.width = 400;
+	canvas.height = 300;
+	
+	//add the canvas to the HTML document
+	document.createElement("canvas");
 
 })();
